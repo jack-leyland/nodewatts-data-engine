bd382458cad1580f8d90683c026280b07cd2a4b3
jack-leyland <jackjleyland@gmail.com>
2022-07-22 17:42:26 +0100

    colored logging

--- nwengine/log.py
+++ nwengine/log.py
@@ -1,17 +1,38 @@
 import logging
 
+class ColoredFormatter(logging.Formatter):
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    blue = "\x1b[94m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = '[%(levelname)s] %(asctime)s::%(name)s %(message)s'
+
+    FORMATS = {
+        logging.DEBUG: blue + format + reset,
+        logging.INFO: format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset
+    }
+    
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
 def setup_logger(verbose, name):
     logger = logging.getLogger(name)
-    ch = logging.StreamHandler()
     if verbose:
         logger.setLevel(logging.DEBUG)
         ch.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
         ch.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
     formatter = logging.Formatter(
-        '%(asctime)s::%(levelname)s::%(name)s::%(module)s %(message)s'
-    )
+        ColoredFormatter()
+        )
     ch.setFormatter(formatter)
     logger.addHandler(ch)
     return logger
